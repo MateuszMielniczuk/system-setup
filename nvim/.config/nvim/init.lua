f99ac82d180a69b7682fdf7239445848bfb60005
MateuszMielniczuk <matheuszmmi@gmail.com>
2025-06-22 18:19:20 +0000

    Less anoying spell errors

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- make spell errors less annoying
+vim.cmd([[
+  hi clear SpellBad
+  hi SpellBad cterm=underline
+  hi SpellBad gui=undercurl
+  ]])

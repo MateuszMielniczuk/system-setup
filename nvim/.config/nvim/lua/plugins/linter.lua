51054a81e3e9a2779a5cc3663e10e954a5d49e52
MateuszMielniczuk <matheuszmmi@gmail.com>
2025-11-22 12:37:31 +0000

    update lazy to v15

--- nvim/.config/nvim/lua/plugins/linter.lua
+++ nvim/.config/nvim/lua/plugins/linter.lua
@@ -14,31 +14,33 @@ return {
         "mason-org/mason.nvim",
         opts = {
             ensure_installed = {
-                "stylua",
-                "shellcheck",
-                "shfmt",
-                "flake8",
                 "bash-language-server",
                 "css-lsp",
                 "docker-compose-language-service",
                 "dockerfile-language-server",
-                "emmet-ls",
-                "eslint-lsp",
+                "emmet-ls", -- for html and css
+                "eslint-lsp", -- js/ts linter
+                "flake8", -- python linter
                 "html-lsp",
-                "jedi-language-server",
+                "jedi-language-server", -- python language server
                 "json-lsp",
                 "lua-language-server",
-                "marksman",
-                "phpactor",
-                "pyright",
-                "ruff",
+                "marksman", -- markdown linter
+                "mmdc", -- mermaid cli
+                "phpactor", -- php language server
+                "pyright", -- python language server
+                "ruff", -- python linter
                 "rust-analyzer",
-                "tailwindcss-language-server",
-                "taplo",
+                "shellcheck", -- shell script linter
+                "shfmt", -- shell script formatter
+                "stylua", -- lua formatter
+                "tailwindcss-language-server", -- tailwindcss lsp
+                "taplo", -- toml lsp
+                "tectonic", -- markdown compiler
                 "typescript-language-server",
-                "vtsls",
-                "vue-language-server",
-                "yaml-language-server",
+                "vtsls", -- ts/js language server
+                "vue-language-server", -- vue lsp
+                "yaml-language-server", -- yaml lsp
             },
         },
     },
